d4bb29f32e51b531c8403fb04f198c9c48eed86c
qiuxirufeng <2290932816@qq.com>
2019-08-20 08:02:01 +0800

    blog的管理后台 	删除__pycache__ 	删除__pycache__ 	model的__str__方法 	删除__pycache__ 	删除__pycache__ 	删除__pycache__ 	删除__pycache__ 	删除__pycache__ 	删除__pycache__ 	删除__pycache__

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import  format_html
 
-# Register your models here.
+from .models import Post, Category, Tag
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
+    fields = ('name', 'status', 'is_nav')
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+
+    post_count.short_description = '文章数量'
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'category', 'status',
+        'created_time', 'owner', 'operator'
+    ]
+    list_display_links = []
+
+    list_filter = ['category', ]
+    search_fields = ['title', 'category__name']
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    # 编辑页面
+    save_on_top = True
+
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('admin:blog_post_change', args=(obj.id,))
+        )
+    operator.short_description = '操作'
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = '分类'
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     STATUS_NORMAL = 1
@@ -34,6 +37,9 @@ class Tag(models.Model):
 
     class Meta:
         verbose_name = verbose_name_plural = '标签'
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -58,3 +64,6 @@ class Post(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = "文章"
         ordering = ['-id']      # 根据id进行降序排列
+
+    def __str__(self):
+        return self.title
