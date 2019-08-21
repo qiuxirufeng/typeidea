65f3fc20c9fb95d826c44ce45b050961031207b1
qiuxirufeng <2290932816@qq.com>
2019-08-21 23:05:22 +0800

    抽象Admin基类

--- blog/admin.py
+++ blog/admin.py
@@ -2,13 +2,21 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
-from .models import Post, Category, Tag
 from .adminforms import PostAdminForm
+from .models import Post, Category, Tag
+from typeidea.base_admin import BaseOwnerAdmin
 from typeidea.custom_site import custom_site
 
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class PostInline(admin.TabularInline):
+    fields = ('title', 'desc')
+    extra = 1   # 控制额外多几个
+    model = Post
+
+
+@admin.register(Category, site=custom_site)
+class CategoryAdmin(BaseOwnerAdmin):
+    inlines = [PostInline, ]
     list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
     fields = ('name', 'status', 'is_nav')
 
@@ -17,19 +25,11 @@ class CategoryAdmin(admin.ModelAdmin):
 
     post_count.short_description = '文章数量'
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)
 
-
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+@admin.register(Tag, site=custom_site)
+class TagAdmin(BaseOwnerAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status')
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
 class CategoryOwnerFilter(admin.SimpleListFilter):
@@ -52,14 +52,12 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
 @admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
     form = PostAdminForm
-
     list_display = [
         'title', 'category', 'status',
         'created_time', 'owner', 'operator'
     ]
     list_display_links = []
 
-    # list_filter = ['category', ]
     list_filter = [CategoryOwnerFilter]
     search_fields = ['title', 'category__name']
 
@@ -97,7 +95,8 @@ class PostAdmin(admin.ModelAdmin):
             'fields': ('tag', ),
         })
     )
-
+    # filter_horizontal = ('tag', )
+    filter_vertical = ('tag', )
 
     def operator(self, obj):
         return format_html(
@@ -106,13 +105,6 @@ class PostAdmin(admin.ModelAdmin):
         )
     operator.short_description = '操作'
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(PostAdmin, self).save_model(request, obj, form, change)
-
-    def get_queryset(self, request):
-        qs = super(PostAdmin, self).get_queryset(request)
-        return qs.filter(owner=request.user)
 
     # class Media:
     #     css = {
--- config/admin.py
+++ config/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
 from .models import Link, SideBar
+from typeidea.custom_site import custom_site
+from typeidea.base_admin import BaseOwnerAdmin
 
-@admin.register(Link)
-class LinkAdmin(admin.ModelAdmin):
+
+@admin.register(Link, site=custom_site)
+class LinkAdmin(BaseOwnerAdmin):
     list_display = ('title', 'href', 'status', 'weight', 'created_time')
     fields = ('title', 'href', 'status', 'weight')
 
@@ -12,8 +15,8 @@ class LinkAdmin(admin.ModelAdmin):
         return super(LinkAdmin, self).save_model(request, obj, form, change)
 
 
-@admin.register(SideBar)
-class SideBarAdmin(admin.ModelAdmin):
+@admin.register(SideBar, site=custom_site)
+class SideBarAdmin(BaseOwnerAdmin):
     list_display = ('title', 'display_type', 'content', 'created_time')
     fields = ('title', 'display_type', 'content')
 
